e5c0f324ceaf7e91d06250efe3baa0d889e31172
Nika <nshub24@freeuni.edu.ge>
2026-03-05 12:11:35 +0400

    seminar 3 Pair code

--- seminar1/src/main/java/seminar3/Pair.java
+++ seminar1/src/main/java/seminar3/Pair.java
@@ -1,5 +1,51 @@
 package seminar3;
 
-public class Pair {
+import java.util.Objects;
+
+public class Pair<K, V> {
+    private K key;
+    private V value;
+
+    public Pair() {};
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Pair)) return false;
+        Pair<?, ?> pr = (Pair<?, ?>) o;
+        return Objects.equals(key, pr.key) && Objects.equals(value, pr.value);
+    }
+
+    ///  yoveltvis tu gansxvavebulad ginda hashcode da toString daao,plementeeeeeeeeeeeeee
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, value);
+    }
+
+    @Override
+    public String toString() {
+        return key + "=" + value;
+    }
 
 }
--- seminar1/src/test/java/PairTest.java
+++ seminar1/src/test/java/PairTest.java
@@ -1,2 +1,41 @@
+import org.junit.jupiter.api.Test;
+import seminar3.Pair;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PairTest {
+    @Test
+    public void testSetValue() {
+        Pair<String, Integer> pr = new Pair<>("key", 5);
+        pr.setValue(55);
+        assertEquals(55, pr.getValue());
+    }
+
+    @Test
+    public void testSetKey() {
+        Pair<String, Integer> pr = new Pair<>("key", 5);
+        pr.setKey("aa");
+        assertEquals("aa", pr.getKey());
+    }
+
+    @Test
+    public void testGetValue() {
+        Pair<String, Integer> pr = new Pair<>("key", 5);
+        assertEquals("key", pr.getKey());
+    }
+
+    @Test
+    public void testGetKey() {
+        Pair<String, Integer> pr = new Pair<>("key", 5);
+        assertEquals(5, pr.getValue());
+    }
+
+    @Test
+    public void testEqual() {
+        Pair<Integer, String> pr1 = new Pair<>(1, "a");
+        Pair<Integer, String> pr2 = new Pair<>(1, "a");
+        assertEquals(pr1, pr2);
+        assertEquals(pr2, pr1);
+
+    }
 }
